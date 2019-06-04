6dadc4dec81c80da783bea88149f7c9975244396
atplant <32179010+atplant@users.noreply.github.com>
2019-06-03 22:09:59 -0700

    Update Triggers.sql

--- Triggers.sql
+++ Triggers.sql
@@ -1,10 +1,4 @@
-CREATE TRIGGER clean_playlists
-AFTER
-DELETE
-ON Users u
-FOR EACH ROW
-DELETE ON Playlists p
-WHERE p.Created_by = u.Username;
+--After a User is removed from the database, their Friend list is deleted and they are removed from all their former friends friend lists
 
 CREATE TRIGGER clean_friends
 AFTER
@@ -13,3 +7,14 @@ ON Users u
 FOR EACH ROW
 DELETE ON User_friend f
 WHERE f.Username = u.Username AND WHERE f.Friendname = u.Username;
+
+--After a User is removed from the database, their old playlists are deleted
+
+CREATE TRIGGER clean_playlists
+AFTER
+DELETE
+ON Users u
+FOR EACH ROW
+DELETE ON Playlists p
+WHERE p.Created_by = u.Username;
+
