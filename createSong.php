744ab5988bb964f6492c1ea928d0586a5b1d4bd5
atplant <32179010+atplant@users.noreply.github.com>
2019-06-09 20:13:34 -0700

    Update createSong.php

--- createSong.php
+++ createSong.php
@@ -67,8 +67,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
 <head>
     <meta charset="UTF-8">
     <title>Create Record</title>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
-	<script src="style.css"></script>
+    <link rel="stylesheet" href="style.css">
     <style type="text/css">
         .wrapper{
             width: 500px;
